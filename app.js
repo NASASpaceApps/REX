6f184eeba35522c874b04679e376c89437a1c3d3
Adam Wootton <superadamwo@hotmail.com>
2013-04-19 22:13:46 -0400

    added a landing page

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
+  //app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
