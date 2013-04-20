226d3e927ffad0528f15bd468d5bac1c10ef7356
VicV <victorvuc@gmail.com>
2013-04-20 09:49:56 -0400

    stuff

---
+++ public/javascripts/location.js
@@ -0,0 +1,37 @@
+var geocoder, results_found = false;
+
+function codeAddress(address, map_canvas) {
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+      $("#long").val(results[0].geometry.location.Za);
+      $("#lat").val(results[0].geometry.location.$a);
+    } else {
+      console.log('Geocode was not successful for the following reason: ' + status + ' blah: ' + address);
+    }
+  });
+}
+
+$(function(){
+  console.log("vbkaslkfnas");
+  map_canvas = $("#map_canvas");
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(43.4701302, -80.5357712);
+  var mapOptions = {
+    zoom: 12,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  map = new google.maps.Map(map_canvas[0], mapOptions);
+
+  $("#location").keydown(function(e){
+    if (e.which == 13) {
+      e.preventDefault();
+      codeAddress($(this).val(), map_canvas);
+    }
+  });
+});
