ab152558943bd9e67c54e6b54d13b4db15b35f9e
VicV <victorvuc@gmail.com>
2013-04-20 17:38:59 -0400

    added a little back button so you can go back in time

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Somethingrather' });
+  res.render('summary', { title: 'Somethingrather' });
 };
 exports.ajax = function(req, res){ 
 	res.render('ajax', {});
